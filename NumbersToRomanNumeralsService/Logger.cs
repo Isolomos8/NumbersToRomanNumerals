9d41bd451190bd3b73f8ee08ae66e540e3c7ad61
Isolomos8 <31577362+Isolomos8@users.noreply.github.com>
2017-09-03 12:05:02 +0200

    Task 2

--- NumbersToRomanNumeralsService/Logger.cs
+++ NumbersToRomanNumeralsService/Logger.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,9 +10,20 @@ namespace NumbersToRomanNumeralsService
     public static class Logger
     {
         private const string Colon = " : ";
+        private const string WhiteSpace = " ";
         public static void Log(string message)
         {
             File.AppendAllText(ServiceConfiguration.LogFileLocation, DateTime.Now.ToString(ServiceConfiguration.TimestampFormat) + Colon + message + Environment.NewLine);
+        }
+
+        public static void LogNewLine()
+        {
+            File.AppendAllText(ServiceConfiguration.LogFileLocation, Environment.NewLine);
+        }
+
+        public static void LogOnSameLine(string message)
+        {
+            File.AppendAllText(ServiceConfiguration.LogFileLocation, message + WhiteSpace);
         }
     }
 }
